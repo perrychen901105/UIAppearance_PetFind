909eca25de6b95ab19e45814c041d4912b83faba
Perry <chenzhipeng901105@gmail.com>
2015-09-14 18:47:20 +0800

    add theme code

---
+++ Pet Finder_Starter/Theme.swift
@@ -0,0 +1,34 @@
+//
+//  Theme.swift
+//  Pet Finder
+//
+//  Created by PerryChen on 9/14/15.
+//  Copyright (c) 2015 Ray Wenderlich. All rights reserved.
+//
+
+import UIKit
+
+enum Theme: Int {
+    case Default, Dark, Graphical
+    
+    var mainColor: UIColor {
+        switch self {
+        case .Default:
+            return UIColor(red: 87.0/255.0, green: 188.0/255.0, blue: 95.0/255.0, alpha: 1.0)
+        case .Dark:
+            return UIColor(red: 242.0/255.0, green: 101.0/255.0, blue: 34.0/255.0, alpha: 1.0)
+        case .Graphical:
+            return UIColor(red: 10.0/255.0, green: 10.0/255.0, blue: 10.0/255.0, alpha: 1.0)
+        }
+    }
+}
+let SelectedThemeKey = "SelectedTheme"
+struct ThemeManager {
+    static func currentTheme() -> Theme {
+        if let storedTheme = NSUserDefaults.standardUserDefaults().valueForKey(SelectedThemeKey)?.integerValue {
+            return Theme(rawValue: storedTheme)!
+        } else {
+            return .Default
+        }
+    }
+}
